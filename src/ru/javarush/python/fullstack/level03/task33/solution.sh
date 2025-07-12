b2cf5862bfb46a4e9b95e2fb55236fa80639b63e
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-07-12 17:08:24 +0300

    add solution level 03 task 33

--- src/ru/javarush/python/fullstack/level03/task33/solution.sh
+++ src/ru/javarush/python/fullstack/level03/task33/solution.sh
@@ -1,8 +1,8 @@
 # Сборка Docker-образа с вашим именем пользователя Docker Hub:
-
+docker build . -t oldsorcerer/my_python_app:1.0
 
 # Авторизация в Docker Hub:
-
+docker login -u oldsorcerer
 
 # Публикация образа в Docker Hub:
-
+docker push oldsorcerer/my_python_app:1.0

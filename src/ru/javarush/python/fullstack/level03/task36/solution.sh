982e2933e77cfaf8b14bd17d8e343a65d11e653d
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-07-12 17:08:40 +0300

    add solution level 03 task 36

--- src/ru/javarush/python/fullstack/level03/task36/solution.sh
+++ src/ru/javarush/python/fullstack/level03/task36/solution.sh
@@ -1,20 +1,22 @@
 # Скопируйте app_v1.py в app.py (для сборки версии 1.0).
+cp app_v1.py app.py
 
 # Соберите образ версии 1.0:
-
-
+docker build . -t oldsorcerer/myapp:1.0
 
 # Скопируйте app_v2.py в app.py (для сборки версии 2.0).
+cp app_v2.py app.py
 
 # Соберите образ версии 2.0:
-
-
+docker build . -t oldsorcerer/myapp:2.0
 
 # Аутентификация в Docker Hub (это потребует ввода учетных данных)
-
+docker login -u oldsorcerer
 
 # Публикация образов в Docker Hub:
-
+docker push oldsorcerer/myapp:1.0
+docker push oldsorcerer/myapp:2.0
 
 # Запуск контейнеров для каждой версии:
-
+docker run --name myappv1 oldsorcerer/myapp:1.0
+docker run --name myappv2 oldsorcerer/myapp:2.0

caf30af258c213cf6da5041179c9abaf4d136b4b
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-07-12 17:08:10 +0300

    add solution level 03 task 30

--- src/ru/javarush/python/fullstack/level03/task30/solution.sh
+++ src/ru/javarush/python/fullstack/level03/task30/solution.sh
@@ -1,9 +1,9 @@
 # Сборка Docker-образа с тегом myapp:2.0
-
+docker build . -t myapp:2.0
 
 # Присвоение дополнительного тега myapp:latest к тому же образу
-
+docker tag myapp:2.0 myapp:latest
 
 # Проверка обоих тегов
-
-
+docker run --rm myapp:2.0
+docker run --rm myapp:latest

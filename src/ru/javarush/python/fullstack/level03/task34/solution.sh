6e4b8de4fab69509992387522116563bafb641a5
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-07-12 17:08:28 +0300

    add solution level 03 task 34

--- src/ru/javarush/python/fullstack/level03/task34/solution.sh
+++ src/ru/javarush/python/fullstack/level03/task34/solution.sh
@@ -1,4 +1,5 @@
 # Загрузка образа nginx:latest из Docker Hub
-
+docker pull nginx:latest
 
 # Запуск контейнера с переадресацией порта 80 контейнера на порт 8080 локальной машины
+docker run -d --name my_nginx -p 8080:80 nginx:latest

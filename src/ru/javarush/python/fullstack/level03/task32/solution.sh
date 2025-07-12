df93a6a9930bc514e2d31d26b6e28515b7bca7d9
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-07-12 17:08:19 +0300

    add solution level 03 task 32

--- src/ru/javarush/python/fullstack/level03/task32/solution.sh
+++ src/ru/javarush/python/fullstack/level03/task32/solution.sh
@@ -1,7 +1,7 @@
 # Сборка и запуск образа для разработки
-
-
+docker build . --build-arg ENVIRONMENT=development -t myapp:dev
+docker run --rm myapp:dev
 
 # Сборка и запуск образа для продакшн
-
-
+docker build . --build-arg ENVIRONMENT=production -t myapp:prod
+docker run --rm myapp:prod

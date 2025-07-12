9cbb082d81d98495cab05f00b0eeb0fbe2d4e812
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-07-12 17:08:34 +0300

    add solution level 03 task 35

--- src/ru/javarush/python/fullstack/level03/task35/solution.sh
+++ src/ru/javarush/python/fullstack/level03/task35/solution.sh
@@ -1,8 +1,8 @@
 # Построение обновленного образа
-
+docker build . -t oldsorcerer/mynodeapp:2.0
 
 # Аутентификация в Docker Hub (это потребует ввода учетных данных)
-
+docker login -u oldsorcerer
 
 # Публикация образа в Docker Hub
-
+docker push oldsorcerer/mynodeapp:2.0

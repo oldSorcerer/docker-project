415440c3120650bca9170eac0ecb734fc7e90209
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-07-19 18:29:23 +0300

    add solution level 04 task 05

--- src/ru/javarush/python/fullstack/level04/task05/solution.sh
+++ src/ru/javarush/python/fullstack/level04/task05/solution.sh
@@ -16,16 +16,17 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Устанавливаем Docker Engine
-
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin
 
 # Добавляем текущего пользователя в группу docker для использования Docker без sudo
-
+sudo usermod -aG docker $USER
 
 # Активируем изменения в группах пользователя (потребуется выйти и зайти снова)
-
+newgrp docker
 
 # Устанавливаем Docker Compose как плагин
-
+sudo apt-get install -y docker-compose-plugin
 
 # Проверяем установку Docker Compose, отображая его версию
-
+docker compose version

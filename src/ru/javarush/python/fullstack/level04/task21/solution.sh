92666c33145066f063a543dcbaad8d33b0040ae8
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-07-19 18:31:25 +0300

    add solution level 04 task 21

--- src/ru/javarush/python/fullstack/level04/task21/solution.sh
+++ src/ru/javarush/python/fullstack/level04/task21/solution.sh
@@ -1,4 +1,9 @@
 # Запуск для разработки
-
+docker compose --env-file .env.development up
+echo "Development environment is running on port 8080"
+docker compose down
 
 # Запуск для продакшена
+docker compose --env-file .env.production up
+echo "Production environment is running on port 80"
+docker compose down

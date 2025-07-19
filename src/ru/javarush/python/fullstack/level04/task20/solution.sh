7676285b101605f0bae2982cacbd9e3cfa3cdae7
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-07-19 18:31:05 +0300

    add solution level 04 task 20

--- src/ru/javarush/python/fullstack/level04/task20/solution.sh
+++ src/ru/javarush/python/fullstack/level04/task20/solution.sh
@@ -1,4 +1,6 @@
 # Установите необходимые переменные окружения с помощью команды `export`:
-
+export NGINX_VERSION=latest
+export HOST_PORT=8080
 
 # После установки переменных запустите Docker Compose:
+docker compose up

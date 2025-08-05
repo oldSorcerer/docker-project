3559e3f11fbec955c455d9507d1c3b6e039ccff5
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-05 13:10:17 +0300

    add solution level 05 task 06

--- src/ru/javarush/python/fullstack/level05/task06/solution.sh
+++ src/ru/javarush/python/fullstack/level05/task06/solution.sh
@@ -1,4 +1,5 @@
 # Запускаем контейнер my_container без подключения к какой-либо сети
-
+docker run -d --name my_container nginx
 
 # Подключаем контейнер my_container к существующей сети my_bridge_network
+docker network connect my_bridge_network my_container

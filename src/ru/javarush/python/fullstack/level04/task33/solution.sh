ee0e92146259abfbc4a8dd31d31a0b95c3db46a6
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-05 13:09:39 +0300

    add solution level 04 task 33

--- src/ru/javarush/python/fullstack/level04/task33/solution.sh
+++ src/ru/javarush/python/fullstack/level04/task33/solution.sh
@@ -1,11 +1,16 @@
 # Запуск контейнеров
-docker compose up
+docker compose up -d
 
 # Пауза для ожидания запуска контейнера
 sleep 10
 
 # Подключение и проверка данных
-docker compose exec -it p
+winpty docker exec -it postgres_container psql -U exampleuser -d exampledb -c "\l"
 
 # Остановка контейнеров после проверки
 docker compose down
+
+#psql — запускает клиентскую утилиту PostgreSQL внутри контейнера.
+#-U exampleuser — пользователь базы данных.
+#-d exampledb — имя базы данных.
+#-c "\l" — команда для psql: выведет список всех баз данных (то есть выполнит мета-команду \l).

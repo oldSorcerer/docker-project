835fea83179c89a3a92a2aa9eb25c05a917ab26e
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-05 13:10:26 +0300

    add solution level 05 task 08

--- src/ru/javarush/python/fullstack/level05/task08/solution.sh
+++ src/ru/javarush/python/fullstack/level05/task08/solution.sh
@@ -1,10 +1,11 @@
 # Создание сети с драйвером bridge
-
+docker network create --driver bridge my_bridge_network
 
 # Запуск контейнера и подключение его к сети
-
+docker run -d --name container1 --network my_bridge_network nginx
 
 # Отключение контейнера от сети
-
+docker network disconnect my_bridge_network container1
 
 # Удаление созданной сети
+docker network rm my_bridge_network

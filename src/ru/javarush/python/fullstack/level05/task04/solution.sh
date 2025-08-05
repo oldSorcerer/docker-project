f4cdff6c6f18989431575b2fa708d8c043da3813
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-05 13:10:08 +0300

    add solution level 05 task 04

--- src/ru/javarush/python/fullstack/level05/task04/solution.sh
+++ src/ru/javarush/python/fullstack/level05/task04/solution.sh
@@ -1,11 +1,13 @@
 # Инициализация Docker Swarm
-
+docker swarm init
 
 # Создание overlay-сети
-
+docker network create --driver overlay my_overlay_network
 
 # Развертывание сервиса с контейнером Nginx в overlay-сети
-
+docker service create --name nginx_service --network my_overlay_network -p 8080:80 nginx
 
 # Проверка работы сервиса
+docker service ls
 
+#docker service rm nginx_service

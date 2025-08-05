c6c423fb7a94c2a32d0c3b3d0458d0cbf3af92dd
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-05 13:10:12 +0300

    add solution level 05 task 05

--- src/ru/javarush/python/fullstack/level05/task05/solution.sh
+++ src/ru/javarush/python/fullstack/level05/task05/solution.sh
@@ -1,10 +1,11 @@
 # Создание новой сети с драйвером bridge
-
+docker network create --driver bridge my_bridge_network
 
 # Запуск контейнера с Nginx и подключение его к сети
-
+docker run -d --name nginx_container --network my_bridge_network nginx
 
 # Запуск контейнера с Redis и подключение его к сети
-
+docker run -d --name redis_container --network my_bridge_network redis
 
 # Проверка работы контейнеров в сети
+docker network inspect my_bridge_network

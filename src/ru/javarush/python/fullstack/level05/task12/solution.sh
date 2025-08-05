1757750cc30937e4efe0252b4fa24e6a4067448c
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-05 13:10:43 +0300

    add solution level 05 task 12

--- src/ru/javarush/python/fullstack/level05/task12/solution.sh
+++ src/ru/javarush/python/fullstack/level05/task12/solution.sh
@@ -1,4 +1,14 @@
 # Запуск контейнеров с помощью Docker Compose
+docker compose up -d
 
+sleep 3
+
+winpty docker exec nginx_container apt-get update
+winpty docker exec nginx_container apt-get install -y iputils-ping
+# обновление и установка в контейнер утилиты ping
 
 # Проверка связи между веб-сервером и базой данных
+docker network inspect task12_my_bridge_network
+winpty docker exec nginx_container ping -c 4 postgres_container
+
+sleep 10

199b1e26bac87ee28a025c083beb4aa615282395
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-06 13:11:53 +0300

    add solution level 05 task 21

--- src/ru/javarush/python/fullstack/level05/task21/solution.sh
+++ src/ru/javarush/python/fullstack/level05/task21/solution.sh
@@ -1,4 +1,5 @@
 # Инициализируем Docker Swarm на хосте с IP-адресом 192.168.1.10
-
+docker swarm init --advertise-addr 192.168.1.10
 
 # Получаем команду для присоединения рабочих узлов
+docker swarm join-token worker

d96f717b5c49fb6f9b10001aed49bf449b77bf64
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-06 13:11:36 +0300

    add solution level 05 task 17

--- src/ru/javarush/python/fullstack/level05/task17/solution.sh
+++ src/ru/javarush/python/fullstack/level05/task17/solution.sh
@@ -1,10 +1,11 @@
 # Создание пользовательской сети типа bridge с именем my_bridge_network
-
+docker network create --name my_bridge_network
 
 # Запуск контейнера с Nginx и подключение его к my_bridge_network
-
+docker run -d --name nginx_container --network my_bridge_network nginx
 
 # Запуск контейнера с Busybox и подключение его к my_bridge_network
-
+docker run -d --name busybox_container --network my_bridge_network busybox sleep 3600
 
 # Проверка связи между контейнерами с помощью команды ping
+docker exec busybox_container ping -c 4 nginx_container

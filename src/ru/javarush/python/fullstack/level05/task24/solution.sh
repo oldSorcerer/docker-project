974b1e83e0c248bc5ee90553c108e76eaceacb5e
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-06 13:12:08 +0300

    add solution level 05 task 24

--- src/ru/javarush/python/fullstack/level05/task24/solution.sh
+++ src/ru/javarush/python/fullstack/level05/task24/solution.sh
@@ -1,7 +1,14 @@
 # Инициализация Docker Swarm 
-
+docker swarm init
 
 # Загрузите последний образ Nginx
-
+docker pull nginx:latest
 
 # Обновите сервис my_web, чтобы он использовал последнюю версию образа Nginx
+docker service update --image nginx:latest --update-order start-first my_web
+
+#--update-order start-first
+# определяет порядок обновления реплик при деплое сервиса в Docker Swarm
+
+#docker swarm leave --force
+# принудительно исключает текущую ноду из Docker Swarm кластера

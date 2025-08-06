bffaf0b04633c818d292f2e8929403b003fe7b4d
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-06 13:12:28 +0300

    add solution level 05 task 28

--- src/ru/javarush/python/fullstack/level05/task28/solution.sh
+++ src/ru/javarush/python/fullstack/level05/task28/solution.sh
@@ -1,10 +1,15 @@
 # Инициализация Docker Swarm
-
+docker swarm init
 
 # Создание сети
-
+docker network create -d overlay my_overlay_network
 
 # Развертывание стека с помощью Docker Compose
-
+docker stack deploy -c docker-compose.yml my_stack
 
 # Проверка развернутых сервисов
+docker stack services my_stack
+
+sleep 25
+docker swarm leave --force
+# принудительно исключает текущую ноду из Docker Swarm кластера

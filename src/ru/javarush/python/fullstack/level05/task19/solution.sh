8c76f1c385fc297e152ce0f84e45463f0641cd78
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-06 13:11:44 +0300

    add solution level 05 task 19

--- src/ru/javarush/python/fullstack/level05/task19/solution.sh
+++ src/ru/javarush/python/fullstack/level05/task19/solution.sh
@@ -1,13 +1,17 @@
 # Создание сети macvlan
-
+docker network create -d macvlan \
+--subnet=192.168.1.0/24 \
+--gateway=192.168.1.1 \
+-o parent=eth0 my_macvlan_network
 
 # Запуск контейнера с Nginx и подключение к сети macvlan
-
+docker run -d --name nginx_container --network my_macvlan_network nginx
 
 # Запуск контейнера с Busybox и подключение к сети macvlan
-
+docker run -d --name busybox_container --network my_macvlan_network busybox sleep 3600
 
 # Проверка связи между контейнерами с помощью команды ping
-
+docker exec -it busybox_container ping -c 4 nginx_container
 
 # Проверка сети
+docker network inspect my_macvlan_network

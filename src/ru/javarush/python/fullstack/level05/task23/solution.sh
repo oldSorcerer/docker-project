95c2f8eccabd4dc3cff292df8d307ef8c85a84ed
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-06 13:12:02 +0300

    add solution level 05 task 23

--- src/ru/javarush/python/fullstack/level05/task23/solution.sh
+++ src/ru/javarush/python/fullstack/level05/task23/solution.sh
@@ -1,7 +1,11 @@
 # Инициализация Docker Swarm
-
+docker swarm init
 
 # Создание сервиса my_web с 3 репликами
-
+docker service create --name my_web --replicas 3 -p 8080:80 nginx:1.28.0
 
 # Масштабирование сервиса my_web до 5 реплик
+docker service scale my_web=5
+
+#docker swarm leave --force
+# принудительно исключает текущую ноду из Docker Swarm кластера

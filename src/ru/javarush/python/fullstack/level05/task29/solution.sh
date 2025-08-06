f4baa46ec15c60dab0a2f7355d6783b4bb997290
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-06 13:12:35 +0300

    add solution level 05 task 29

--- src/ru/javarush/python/fullstack/level05/task29/solution.sh
+++ src/ru/javarush/python/fullstack/level05/task29/solution.sh
@@ -1,11 +1,17 @@
 # Создание сети
-
+docker network create webnet
 
 # Запуск контейнера web с Nginx, подключенного к сети webnet
+docker run -d --name web --network webnet nginx
 
+winpty docker exec web apt-get update
+winpty docker exec web apt-get install -y iputils-ping
+# обновление и установка в контейнер утилиты ping
 
 # Запуск контейнера db с PostgreSQL, подключенного к сети webnet
-
+docker run -d --name db --network webnet -e POSTGRES_PASSWORD=mypassword postgres
 
 # Проверка связи с помощью ping
+docker exec -it web ping -c 4 db
 
+sleep 10

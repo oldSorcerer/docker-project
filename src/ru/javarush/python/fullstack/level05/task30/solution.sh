c617ecd932eb4a9d18d6d9e78a578c9c8cad644d
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-07 13:12:50 +0300

    add solution level 05 task 30

--- src/ru/javarush/python/fullstack/level05/task30/solution.sh
+++ src/ru/javarush/python/fullstack/level05/task30/solution.sh
@@ -1,18 +1,20 @@
 # Убедитесь, что сеть webnet создана
-
+docker network create webnet
 
 # Запуск контейнера web с Nginx, подключенного к сети webnet
-
+docker run -d --name web --network webnet nginx
 
 # Запуск контейнера db с PostgreSQL, подключенного к сети webnet
-
+docker run -d --name db --network webnet -e POSTGRES_PASSWORD=mypassword postgres
 
 # Установка утилиты nslookup в контейнере web
-
-
+docker exec web apt-get update
+docker exec web apt-get install -y dnsutils
 
 # Проверка DNS с помощью nslookup
-
+docker exec -it web nslookup db
 
 # Проверьте, что оба контейнера подключены к одной и той же сети
+docker network inspect webnet
 
+sleep 10

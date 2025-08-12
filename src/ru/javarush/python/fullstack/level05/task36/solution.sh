9170537a84c45c8b8e0ad632fe7ce029ac6b7a69
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-12 21:59:08 +0300

    add solution level 05 task 36

--- src/ru/javarush/python/fullstack/level05/task36/solution.sh
+++ src/ru/javarush/python/fullstack/level05/task36/solution.sh
@@ -1,1 +1,6 @@
 # Запустите контейнер с Nginx, смонтировав сертификаты и ключи для использования TLS
+docker run -d -p 443:443 --name nginx_tls \
+  -v /docker/cert.pem:/etc/nginx/cert.pem \
+  -v /docker/key.pem:/etc/nginx/key.pem \
+  -v /docker/nginx.conf:/etc/nginx/nginx.conf \
+  nginx

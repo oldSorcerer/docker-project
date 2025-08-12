b72c741ae76ade50cea005aee230dc029849d2ce
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-12 21:58:40 +0300

    add solution level 05 task 33

--- src/ru/javarush/python/fullstack/level05/task33/solution.sh
+++ src/ru/javarush/python/fullstack/level05/task33/solution.sh
@@ -1,9 +1,9 @@
 # Создайте пользовательскую bridge-сеть с названием secure_network
-
+docker network create --driver bridge secure_network
 
 # Запустите контейнер с Nginx, подключив его к сети secure_network
 # Контейнер будет изолирован от других сетей и контейнеров
-
+docker run -d --name nginx --network secure_network nginx
 
 # Проверьте, что контейнер запущен и подключен к сети secure_network
-
+docker network inspect secure_network

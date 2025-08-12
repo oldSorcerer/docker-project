1b5b54cca5ba609ccdc8d1b39b74fbcc5f4e6892
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-12 21:58:35 +0300

    add solution level 05 task 32

--- src/ru/javarush/python/fullstack/level05/task32/solution.sh
+++ src/ru/javarush/python/fullstack/level05/task32/solution.sh
@@ -2,16 +2,16 @@
 docker ps
 
 # Остановите контейнер web перед изменением сети
-
+docker stop web
 
 # Создайте новую сеть с названием my_custom_network и подсетью 192.168.2.0/24
-
+docker network create --subnet=192.168.2.0/24 my_custom_network
 
 # Подключите контейнер web к новой сети my_custom_network
-
+docker network connect my_custom_network web
 
 # Запустите контейнер web после подключения к новой сети
-
+docker start web
 
 # Проверьте подключение контейнера web к новой сети
 docker network inspect my_custom_network

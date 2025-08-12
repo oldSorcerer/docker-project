fc86de9b54b3a607040ae9c63ddaad03b6459c51
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-12 21:58:49 +0300

    add solution level 05 task 35

--- src/ru/javarush/python/fullstack/level05/task35/solution.sh
+++ src/ru/javarush/python/fullstack/level05/task35/solution.sh
@@ -1,5 +1,6 @@
 # Запустите контейнер с Nginx
-
+docker run -d --name monitored_nginx nginx
 
 # Включите мониторинг активности контейнера с помощью команды docker events
 # Мониторьте все события, происходящие с контейнером monitored_nginx
+docker events --filter container=monitored_nginx

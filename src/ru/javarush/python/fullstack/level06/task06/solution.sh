4a1e8c9f84b161cfbb75b96a743e71ed5ec58feb
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-13 22:00:02 +0300

    add solution level 06 task 06

--- src/ru/javarush/python/fullstack/level06/task06/solution.sh
+++ src/ru/javarush/python/fullstack/level06/task06/solution.sh
@@ -1,17 +1,17 @@
 # Создайте том с именем logs_volume для хранения логов
-
+docker volume create logs_volume
 
 # Запустите контейнер (например, Nginx), монтируя том logs_volume
-
+docker run -d --name nginx_log_container -v logs_volume:/var/log/nginx nginx
 
 # Остановите контейнер
-
+docker stop nginx_log_container
 
 # Удалите контейнер
-
+docker rm nginx_log_container
 
 # Удалите том logs_volume
-
+docker volume rm logs_volume
 
 # Проверьте, что том больше не существует
-
+docker volume ls

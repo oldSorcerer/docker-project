265444aa5e67486f8cdc4c4a379ecc8ffc2331ee
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-13 21:59:46 +0300

    add solution level 06 task 03

--- src/ru/javarush/python/fullstack/level06/task03/solution.sh
+++ src/ru/javarush/python/fullstack/level06/task03/solution.sh
@@ -1,5 +1,6 @@
 # Создайте том с именем pg_data для хранения данных PostgreSQL
-
+docker volume create pg_data
 
 # Запустите контейнер с PostgreSQL, монтируя том pg_data в директорию /var/lib/postgresql/data контейнера
-
+docker run -d --name postgres -e POSTGRES_PASSWORD=mysecretpassword \
+  -v pg_data:/var/lib/postgresql/data postgres

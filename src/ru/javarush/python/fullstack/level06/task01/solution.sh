feef6ba4f9a329c99294760830870dbebe6cd804
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-13 21:59:36 +0300

    add solution level 06 task 01

--- src/ru/javarush/python/fullstack/level06/task01/solution.sh
+++ src/ru/javarush/python/fullstack/level06/task01/solution.sh
@@ -1,10 +1,11 @@
 # Создайте том с именем my_data_volume
-
+docker volume create my_data_volume
 
 # Запустите контейнер с Nginx, смонтировав том my_data_volume в директорию /data контейнера
-
+docker run -d --name nginx_with_volume -v my_data_volume:/data nginx
 
 # Проверьте, что том подключен к контейнеру
-
+docker inspect nginx_with_volume | grep "Mounts"
 
 # Проверьте, что том создан
+docker volume ls

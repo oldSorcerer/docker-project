b25f4c26bd5bf978ee51a3228ac67a126b12f20a
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-13 21:59:51 +0300

    add solution level 06 task 04

--- src/ru/javarush/python/fullstack/level06/task04/solution.sh
+++ src/ru/javarush/python/fullstack/level06/task04/solution.sh
@@ -1,4 +1,5 @@
 # Создайте том с именем website_data для хранения данных веб-сайта
-
+docker volume create website_data
 
 # Запустите контейнер с Nginx, монтируя том website_data в директорию /usr/share/nginx/html контейнера
+docker run -d --name nginx_container -v website_data:/usr/share/nginx/html -p 8080:80 nginx

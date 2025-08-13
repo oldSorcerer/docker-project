1a129b6188fb1d1fcbba7cc47eb7a7edd307cc5e
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-13 21:59:56 +0300

    add solution level 06 task 05

--- src/ru/javarush/python/fullstack/level06/task05/solution.sh
+++ src/ru/javarush/python/fullstack/level06/task05/solution.sh
@@ -1,7 +1,8 @@
 # Создать том с именем db_backup
-
+docker volume create db_backup
 
 # Запуск временного контейнера с использованием тома db_backup
-
+docker run --rm -v db_backup:/data busybox
 
 # Получение информации о томе db_backup
+docker volume inspect db_backup

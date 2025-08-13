06c1607b035fda5af25d140bc0e7b2e9fb53b1a2
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-13 21:59:42 +0300

    add solution level 06 task 02

--- src/ru/javarush/python/fullstack/level06/task02/solution.sh
+++ src/ru/javarush/python/fullstack/level06/task02/solution.sh
@@ -1,8 +1,11 @@
 # Создаем директорию  /home/user/website
-
+mkdir -p /home/user/website
 
 # Запустите контейнер с Nginx, смонтировав локальную директорию /home/user/website
 # в директорию /usr/share/nginx/html контейнера
-
+docker run -d --name nginx_with_volume \
+  -v /home/user/website:/usr/share/nginx/html \
+   -p 8080:80 nginx
 
 # Проверьте, что контейнер запущен и директория смонтирована
+docker inspect nginx_with_volume | grep "Mounts"

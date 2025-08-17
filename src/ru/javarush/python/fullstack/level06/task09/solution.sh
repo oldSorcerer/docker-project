a30d790d7eb604493b0e8561ee91d24a7d77b4a3
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-17 12:54:13 +0300

    add solution level 06 task 09

--- src/ru/javarush/python/fullstack/level06/task09/solution.sh
+++ src/ru/javarush/python/fullstack/level06/task09/solution.sh
@@ -1,6 +1,7 @@
 # Создайте два тома: data_volume и logs_volume
-
+docker volume create data_volume
+docker volume create logs_volume
 
 # Запустите контейнер с Nginx, монтируя data_volume в директорию /data и logs_volume в директорию /logs
-
+docker run -d --name nginx_container -v data_volume:/data -v logs_volume:/logs -p 8080:80 nginx
 

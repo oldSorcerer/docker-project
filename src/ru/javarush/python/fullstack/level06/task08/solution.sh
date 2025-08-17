b88e81892ce72995dbc1d4e348b1401cc5a26a1b
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-17 12:54:07 +0300

    add solution level 06 task 08

--- src/ru/javarush/python/fullstack/level06/task08/solution.sh
+++ src/ru/javarush/python/fullstack/level06/task08/solution.sh
@@ -1,4 +1,5 @@
 # Создать том
-
+docker volume create app_data
 
 # Запустите контейнер с Nginx, монтируя том app_data в директорию /app внутри контейнера
+docker run -d --name nginx_app -v app_data:/app -p 8080:80 nginx

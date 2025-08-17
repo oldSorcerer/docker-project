acec2cd14d24ad0c248296f930aff6a19f72a6bb
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-17 12:54:17 +0300

    add solution level 06 task 10

--- src/ru/javarush/python/fullstack/level06/task10/solution.sh
+++ src/ru/javarush/python/fullstack/level06/task10/solution.sh
@@ -1,6 +1,6 @@
 # Создайте том с именем readonly_volume
-
+docker volume create readonly_volume
 
 # Запустите контейнер с Nginx, монтируя том readonly_volume в директорию /app в режиме "только для чтения"
-
+docker run -d --name nginx --mount source=readonly_volume,target=/app,readonly -p 8080:80 nginx
 

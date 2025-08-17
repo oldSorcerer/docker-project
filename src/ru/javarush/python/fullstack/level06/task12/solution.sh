89ff5514344d0da70afa51f2fa34ac343faaf4e4
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-17 12:54:29 +0300

    add solution level 06 task 12

--- src/ru/javarush/python/fullstack/level06/task12/solution.sh
+++ src/ru/javarush/python/fullstack/level06/task12/solution.sh
@@ -1,14 +1,14 @@
 # Создайте том с именем app_data
-
+docker volume create app_data
 
 # Запустите контейнер с Nginx, монтируя том app_data для хранения данных
-
+docker run -d --name nginx_app -v app_data:/usr/share/nginx/html -p 8080:80 nginx
 
 # Запишите тестовый файл в том (например, index.html)
-
+docker exec nginx_app sh -c "echo 'Hello Docker!' > /user/share/nginx/html/index.html"
 
 # Создайте бэкап данных тома app_data и сохраните его на хосте в виде архива
-
+docker run --rm -v app_data:/data -v $(pwd):/backup busydox tar -czf /backup/app_data_backup.tar.gz /data
 
 # Проверьте, что бэкап создан в текущей директории
 

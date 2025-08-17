e34906022b15b547fdd885faf7f70690e59bc282
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-17 12:54:23 +0300

    add solution level 06 task 11

--- src/ru/javarush/python/fullstack/level06/task11/solution.sh
+++ src/ru/javarush/python/fullstack/level06/task11/solution.sh
@@ -1,11 +1,13 @@
 # Создайте том с именем config_volume
-
+docker volume create config_volume
 
 # Запустите временный контейнер и запишите в том файл конфигурации
-
+docker run --rm -v config_volume:/config busybox sh -c "echo 'server {listen 80; }' > /config/nginx.conf"
 
 # Запустите еще один временный контейнер, чтобы проверить содержимое тома с помощью команды ls
-
+docker run --rm -v config_volume:/config busybox ls /config
 
 # Вывод содержимого файла конфигурации
+docker run --rm -v config_volume:/config busybox cat /config/nginx.conf
 
+sleep 10

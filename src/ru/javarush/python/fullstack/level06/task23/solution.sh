505d2003b766a75e4f4b8dd534d9d8d9ac50a18f
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-20 09:17:43 +0300

    add solution level 06 task 23

--- src/ru/javarush/python/fullstack/level06/task23/solution.sh
+++ src/ru/javarush/python/fullstack/level06/task23/solution.sh
@@ -1,11 +1,13 @@
 # Остановите и удалите сервис Nginx
-
+docker service rm nginx_service
 
 # Удалите конфигурацию Nginx (например, nginx_config_v1 и nginx_config_v2)
-
+docker config rm nginx_config_v1
+docker config rm nginx_config_v2
 
 # Удалите секрет базы данных PostgreSQL (например, postgres_password)
-
+docker secret rm postgres_password
 
 # Проверьте, что конфигурации и секреты удалены
-
+docker config ls
+docker secret ls

e0488e78f5b9aa31d4354e690dc7aff31b61795d
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-20 09:17:48 +0300

    add solution level 06 task 24

--- src/ru/javarush/python/fullstack/level06/task24/solution.sh
+++ src/ru/javarush/python/fullstack/level06/task24/solution.sh
@@ -1,5 +1,5 @@
 # Создайте резервную копию тома my_volume и сохраните её в файл my_backup.tar.gz
-
+docker-volume-backup backup my_volume my_backup.tar.gz
 
 # Проверьте, что резервная копия создана
 

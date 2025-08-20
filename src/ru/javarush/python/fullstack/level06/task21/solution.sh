974b7355a1cdd3e478edd8b00d54dabc286d0629
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-20 09:17:32 +0300

    add solution level 06 task 21

--- src/ru/javarush/python/fullstack/level06/task21/solution.sh
+++ src/ru/javarush/python/fullstack/level06/task21/solution.sh
@@ -1,4 +1,6 @@
 # Создаем секрет для пароля базы данных PostgreSQL
-
+echo "secret password" | docker secret create db_pass
 
 # Создаем сервис PostgreSQL с использованием созданного секрета
+docker service create --name postgres --secret db_pass \
+-e POSTGRES_PASSWORD_FILE=/run/secrets/db_pass postgres

aa633268c0563ae356a463acb8a1d68016bd9546
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-20 09:17:25 +0300

    add solution level 06 task 20

--- src/ru/javarush/python/fullstack/level06/task20/setup.sh
+++ src/ru/javarush/python/fullstack/level06/task20/setup.sh
@@ -1,4 +1,5 @@
 # Создайте конфигурацию в Docker с помощью команды docker config create
-
+docker config create nginx_config ./nginx.conf
 
 # Создайте сервис Nginx, который будет использовать эту конфигурацию
+docker service create --name nginx --config source=nginx_config,target=/etc/nginx/nginx.conf -p 8080:80 nginx

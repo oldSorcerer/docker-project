f3ab144e18ea1c8e45c25ff1f7e2563c08573b99
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-20 09:17:15 +0300

    add solution level 06 task 18

--- src/ru/javarush/python/fullstack/level06/task18/solution.sh
+++ src/ru/javarush/python/fullstack/level06/task18/solution.sh
@@ -1,1 +1,2 @@
 # Запустите контейнер, смонтировав директорию с хоста в контейнер с правами только для чтения
+docker run --rm --name my_container --mount type=bind,source=/data,target=/app,readonly docker-image

df8308a34e1c8673df155527fe3a968ba85e065a
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-22 16:11:08 +0300

    add solution level 06 task 13

--- src/ru/javarush/python/fullstack/level06/task13/solution.sh
+++ src/ru/javarush/python/fullstack/level06/task13/solution.sh
@@ -4,10 +4,10 @@
 docker volume create app_data
 
 # Восстановите данные из бэкапа в том app_data
-
+docker run --rm -v app_data:/data -v $(pwd):/backup busybox tar -xzf /backup/app_data_backup.tar.gz -C /data
 
 # Запустите контейнер с Nginx, монтируя восстановленный том app_data для использования данных
-
+docker run -d --name nginx_app -v app_data:/usr/share/nginx/html -p 8080:80 nginx
 
 # Проверьте, что контейнер запущен, и данные из тома app_data используются
-
+docker ps

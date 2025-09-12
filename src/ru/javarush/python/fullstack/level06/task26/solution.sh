d9ff67b5be90c71292833bc51ee8e9d9d17f6d51
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-09-12 16:56:08 +0300

    add solution level 06 task 26

--- src/ru/javarush/python/fullstack/level06/task26/solution.sh
+++ src/ru/javarush/python/fullstack/level06/task26/solution.sh
@@ -1,14 +1,17 @@
 # Инициализация репозитория Restic для резервного копирования (если еще не инициализирован)
-
+restic init --repo /path/to/backup/repo
 
 # Выполните резервное копирование данных из директории /path/to/data
-
+restic -r /path/to/backup/repo backup /path/to/data
 
 # Проверьте, что резервное копирование прошло успешно
+restic -r /path/to/backup/repo snapshots
 
+# показывает все снапшоты
 
 # Восстановите данные из последней резервной копии в директорию /path/to/restore
+restic -r /path/to/backup/repo restore latest --target /path/to/restore
 
 
 # Проверьте, что данные были восстановлены в директорию /path/to/restore
-
+ls -l /path/to/restore

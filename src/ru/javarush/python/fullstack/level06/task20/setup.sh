b0be6daa21aa16f0f5331153d1e4b687b25ddfae
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-09-12 16:55:41 +0300

    add solution level 06 task 20

--- src/ru/javarush/python/fullstack/level06/task20/setup.sh
+++ src/ru/javarush/python/fullstack/level06/task20/setup.sh
@@ -1,5 +1,9 @@
+docker swarm init
+
 # Создайте конфигурацию в Docker с помощью команды docker config create
 docker config create nginx_config ./nginx.conf
 
 # Создайте сервис Nginx, который будет использовать эту конфигурацию
-docker service create --name nginx --config source=nginx_config,target=/etc/nginx/nginx.conf -p 8080:80 nginx
+docker service create --name nginx --config source=nginx_config,target=/etc/nginx/nginx.conf -p 8080:80 nginx:alpine
+
+#docker swarm leave --force

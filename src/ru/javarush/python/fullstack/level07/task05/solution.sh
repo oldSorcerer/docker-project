aafa0488beb3a4b7f6df730586b838db245f39c2
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-09-12 16:56:37 +0300

    add solution level 07 task 05

--- src/ru/javarush/python/fullstack/level07/task05/solution.sh
+++ src/ru/javarush/python/fullstack/level07/task05/solution.sh
@@ -1,8 +1,10 @@
 # Сборка Docker-образа
-
+docker build . -t disk_io_app
 
 # Запуск Docker-контейнера
-
+docker run -d --name disk_io_container disk_io_app
 
 # Мониторинг операций ввода/вывода
+docker stats disk_io_container
 
+#sleep 15

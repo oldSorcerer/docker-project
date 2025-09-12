8408295b48c04b9357e0982772be49b50cfd1a5c
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-09-12 16:56:23 +0300

    add solution level 06 task 29

--- src/ru/javarush/python/fullstack/level06/task29/solution.sh
+++ src/ru/javarush/python/fullstack/level06/task29/solution.sh
@@ -1,7 +1,16 @@
 # Создайте секрет для пароля базы данных PostgreSQL
-
+echo "mysecretpassword" | docker secret create postgres_db_password -
 
 # Создайте сеть для сервиса PostgreSQL, если она не создана
-
+docker network create postgres_network
 
 # Создайте сервис PostgreSQL, используя секрет для пароля
+docker service create --name postgres_service \
+  --secret source=postgres_db_password,target=postgres_password \
+  -e POSTGRES_PASSWORD_FILE=/run/secrets/postgres_password \
+  --network postgres_network \
+  postgres
+
+
+# можно как в лекции но лучше использовать source target так как не всегда новый
+# секрет буде там же где и старый

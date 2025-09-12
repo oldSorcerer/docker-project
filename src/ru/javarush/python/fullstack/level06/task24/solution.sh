4eca274aa3e3b4874aa23873242e5bf36948f201
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-09-12 16:55:54 +0300

    add solution level 06 task 24

--- src/ru/javarush/python/fullstack/level06/task24/solution.sh
+++ src/ru/javarush/python/fullstack/level06/task24/solution.sh
@@ -1,11 +1,24 @@
 # Создайте резервную копию тома my_volume и сохраните её в файл my_backup.tar.gz
 docker-volume-backup backup my_volume my_backup.tar.gz
 
-# Проверьте, что резервная копия создана
+# под капотом работает вот так
+# docker run --rm \
+#   -v my_volume:/volume \
+#   -v $(pwd):/backup \
+#   loomchild/volume-backup backup my_backup.tar.gz
 
+# Проверьте, что резервная копия создана
+ls -l my_backup.tar.gz
 
 # Восстановите том my_volume из резервной копии my_backup.tar.gz
+docker-volume-backup restore my_backup.tar.gz my_volume
 
+# docker run --rm \
+#   -v my_volume:/volume \
+#   -v $(pwd):/backup \
+#   loomchild/volume-backup restore my_backup.tar.gz
 
 # Проверьте, что том восстановлен
+docker volume ls | grep my_volume
 
+#docker run --rm -v my_volume:/volume alpine ls /volume

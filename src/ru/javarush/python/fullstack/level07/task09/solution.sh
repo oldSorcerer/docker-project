09dde6eb5165af2ea059f49a18e0484d7c04b727
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-09-15 19:36:49 +0300

    add solution level 07 task 09

--- src/ru/javarush/python/fullstack/level07/task09/solution.sh
+++ src/ru/javarush/python/fullstack/level07/task09/solution.sh
@@ -1,8 +1,11 @@
 # Запуск нескольких контейнеров
-
+docker run -d --name mongo_container mongo
+docker run -d --name redis_container redis
 
 # Сбор метрик с использованием docker stats
-
+docker stats --no-stream --format "table {{.Name}}\t{{.CPUPerc}}\t{{.MemUsage}}"
 
 # Оптимизация потребляемых ресурсов
+docker update --memory 512m mongo_container
+docker update --cpus 0.5 redis_container
 

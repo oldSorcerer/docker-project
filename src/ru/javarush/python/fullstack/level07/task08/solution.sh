86583a8b4a9ae61be49741bf6d94275a4ff94d97
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-09-15 19:36:45 +0300

    add solution level 07 task 08

--- src/ru/javarush/python/fullstack/level07/task08/solution.sh
+++ src/ru/javarush/python/fullstack/level07/task08/solution.sh
@@ -1,9 +1,10 @@
 # Запуск приложения в контейнере
 docker build -t performance-test .
-???
+docker run -d --name app-container performance-test
 
 # Мониторинг производительности контейнера
-??? | tee performance.log
+docker stats app-container --no-stream \
+ --format "table {{.Container}}\t{{.CPUPerc}}\t{{.MemUsage}}" | tee performance.log
 
 # Выборка пиковых значений из логов
 echo "Анализируем данные..."
@@ -11,4 +12,5 @@ grep -Eo '([0-9]+\.[0-9]+)%' performance.log | sort -nr | head -1 | awk '{print 
 grep -Eo '([0-9]+\.[0-9]+MiB)' performance.log | sort -nr | head -1 | awk '{print "Пиковая загрузка памяти: "$1}'
 
 # Завершение работы контейнера после тестирования
-???
+docker stop app-container
+docker rm app-container

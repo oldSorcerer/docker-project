c287c7068dfa4b1e02ea4b5dd460ab73f1378fea
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-09-15 19:36:30 +0300

    add solution level 07 task 06

--- src/ru/javarush/python/fullstack/level07/task06/solution.sh
+++ src/ru/javarush/python/fullstack/level07/task06/solution.sh
@@ -1,7 +1,8 @@
 # Запуск контейнера с Nginx
-
+docker run -d --name nginx -p 8080:80 nginx
 
 # Запуск контейнера с PostgreSQL
-
+docker run -d --name postgres -e POSTGRES_PASSWORD=root -p 5432:5432 postgres
 
 # Мониторинг операций CPU и памяти
+docker stats

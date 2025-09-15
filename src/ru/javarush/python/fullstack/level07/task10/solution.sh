568a5c2423e9fb2e63d9932058fd7b25a1602743
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-09-15 19:36:55 +0300

    add solution level 07 task 10

--- src/ru/javarush/python/fullstack/level07/task10/solution.sh
+++ src/ru/javarush/python/fullstack/level07/task10/solution.sh
@@ -1,8 +1,9 @@
 # Сначала загрузите последнюю версию Prometheus с официального сайта
-
+wget https://github.com/prometheus/prometheus/releases/download/v2.30.3/prometheus-2.30.3.linux-amd64.tar.gz
 
 # Распакуйте архив:
-
+tar -xvfz prometheus-*.tar.gz
 
 # Переместите бинарные файлы в /usr/local/bin:
-
+sudo mv prometheus /usr/local/bin
+sudo mv promtool /usr/local/bin

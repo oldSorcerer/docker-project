119f1bc437f67ec60c7f9df76582816b16f7183c
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-09-17 15:36:16 +0300

    add solution level 07 task 14

--- src/ru/javarush/python/fullstack/level07/task14/solution.sh
+++ src/ru/javarush/python/fullstack/level07/task14/solution.sh
@@ -1,6 +1,7 @@
 # Добавление официального репозитория Grafana
 sudo apt-get install -y software-properties-common
 sudo add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
 
 # Обновление списка пакетов
 sudo apt-get update

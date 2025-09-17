d19a000d0558191961298c42d9f00dce443dac80
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-09-17 15:36:33 +0300

    add solution level 07 task 12

--- src/ru/javarush/python/fullstack/level07/task12/solution.sh
+++ src/ru/javarush/python/fullstack/level07/task12/solution.sh
@@ -1,2 +1,10 @@
 # Запуск cAdvisor в Docker-контейнере
-
+docker run -d \
+  --name=cadvisor \
+  --volume=/:/rootfs:ro \
+  --volume=/var/run:/var/run:ro \
+  --volume=/sys:/sys:ro \
+  --volume=/var/lib/docker/:/var/lib/docker:ro \
+  --publish=8080:8080 \
+  --privileged \
+  google/cadvisor:latest

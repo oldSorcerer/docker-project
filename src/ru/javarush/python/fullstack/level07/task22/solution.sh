906f3b66beccba0c08bf57ace1d1388f8a03cf51
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-04 02:44:47 +0300

    add solution level 07 task 22

--- src/ru/javarush/python/fullstack/level07/task22/solution.sh
+++ src/ru/javarush/python/fullstack/level07/task22/solution.sh
@@ -1,4 +1,6 @@
 # Добавление репозитория Elasticsearch:
+sudo apt-get install -y apt-transport-https
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 sudo sh -c 'echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" > /etc/apt/sources.list.d/elastic-7.x.list'
 
 # Обновление списка пакетов:

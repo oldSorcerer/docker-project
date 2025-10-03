b24557eb0dc21306ea50e2ac46cdad787de1b497
alex <alex@gmail.com>
2025-10-04 02:39:41 +0300

    add solution level 07 task 22

--- src/ru/javarush/python/fullstack/level07/task22/solution.sh
+++ src/ru/javarush/python/fullstack/level07/task22/solution.sh
@@ -1,6 +1,5 @@
 # Добавление репозитория Elasticsearch:
-sudo sh -c 'echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" > \
-/etc/apt/sources.list.d/elastic-7.x.list'
+sudo sh -c 'echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" > /etc/apt/sources.list.d/elastic-7.x.list'
 
 # Обновление списка пакетов:
 sudo apt-get update

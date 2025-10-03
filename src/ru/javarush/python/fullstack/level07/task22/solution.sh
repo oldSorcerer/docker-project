b92cb8515beadaacc120634b5d229a04700e1fc2
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-03 22:45:32 +0300

    add solution level 07 task 22

--- src/ru/javarush/python/fullstack/level07/task22/solution.sh
+++ src/ru/javarush/python/fullstack/level07/task22/solution.sh
@@ -1,13 +1,15 @@
 # Добавление репозитория Elasticsearch:
-
+sudo sh -c 'echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" > \
+/etc/apt/sources.list.d/elastic-7.x.list'
 
 # Обновление списка пакетов:
-
+sudo apt-get update
 
 # Установка Elasticsearch:
-
+sudo apt-get install elasticsearch
 
 # Запуск службы:
-
+sudo systemctl start elasticsearch
 
 # Настройка автозапуска:
+sudo systemctl enable elasticsearch

75c3131551becf1ec49ac65cf03b71d3f9fb949c
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-04 02:57:39 +0300

    add solution level 07 task 23

--- src/ru/javarush/python/fullstack/level07/task23/solution.sh
+++ src/ru/javarush/python/fullstack/level07/task23/solution.sh
@@ -1,15 +1,17 @@
 # Добавление репозитория Logstash:
+sudo apt-get install apt-transport-https
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+sudo sh -c 'echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" > /etc/apt/sources.list.d/elastic-7.x.list'
 
 
 # Обновление списка пакетов:
-
+sudo apt-get update
 
 # Установка Logstash:
-
+sudo apt-get install logstash
 
 # Запуск службы:
-
+sudo systemctl start logstash
 
 # Настройка автозапуска:
-
-
+sudo systemctl enable logstash

b5d3504b69efdde328dce42008317152aa90965f
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-04 03:07:53 +0300

    add solution level 07 task 25

--- src/ru/javarush/python/fullstack/level07/task25/solution.sh
+++ src/ru/javarush/python/fullstack/level07/task25/solution.sh
@@ -4,18 +4,19 @@ wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add
 sudo sh -c 'echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" > /etc/apt/sources.list.d/elastic-7.x.list'
 
 # Обновление списка пакетов:
-
+sudo apt-get update
 
 # Установка Filebeat:
-
+sudo apt-get install filebeat
 
 # Включение модуля Docker в Filebeat:
-
+sudo filebeat modules enable docker
 
 # Запуск службы:
-
+sudo systemctl start filebeat
 
 # Настройка автозапуска:
-
+sudo systemctl enable filebeat
 
 # Проверка работы Filebeat:
+sudo systemctl status filebeat

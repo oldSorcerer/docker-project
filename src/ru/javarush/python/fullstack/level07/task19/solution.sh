651136241f178541e28557ae3f102cb9f4fd01e4
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-23 18:48:59 +0300

    add solution level 07 task 19

--- src/ru/javarush/python/fullstack/level07/task19/solution.sh
+++ src/ru/javarush/python/fullstack/level07/task19/solution.sh
@@ -1,4 +1,5 @@
 # Запуск контейнера с именем my_service
-
+docker run -d --name my_service your_image_name
 
 # Используем команду docker logs с флагом -f для отслеживания логов в режиме реального времени
+docker logs -f my_service
